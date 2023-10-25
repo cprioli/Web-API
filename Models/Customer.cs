a7ed9e59bda79abc66b2ffa944d59bb53344a45f
Caio Prioli <dev.cprioli@outlook.com>
2023-10-24 22:02:50 -0300

    24-10-2023

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCustomer.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
---
+++ ViewModels/CreateCustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCustomer.ViewModels
+{
+    public class CreateCustomerViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
